54fc849af88c4b47560a42b2d8ce8a3b7229f646
DeshuiY <148656746+DeshuiY@users.noreply.github.com>
2024-11-17 19:01:08 -0600

    Fix Move

--- MTEC340_Final/Assets/Scripts/Player/FPSInput.cs
+++ MTEC340_Final/Assets/Scripts/Player/FPSInput.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
 
-// Require a CharacterController component on the object.
-// If no CharacterController component has been attached, it will
-// be automatically added.
 [RequireComponent(typeof(CharacterController))]
-
-// Place object in a given component bin.
 [AddComponentMenu("Control Script/FPS Input")]
-    
 public class FPSInput : MonoBehaviour
 {
     [Range(1.0f, 10.0f)]
@@ -23,30 +17,23 @@ public class FPSInput : MonoBehaviour
 
     private void Update()
     {
-        // Moving the transform via the Translate method won't engage
-        // collision detection.
-
-        // transform.Translate(
-        //    Input.GetAxis("Horizontal") * _speed * Time.deltaTime,
-        //    0,
-        //    Input.GetAxis("Vertical") * _speed * Time.deltaTime);
-
         float deltaX = Input.GetAxis("Horizontal") * _speed;
         float deltaZ = Input.GetAxis("Vertical") * _speed;
-        
-        Vector3 movement = new(deltaX, 0, deltaZ);
-        
-        // Clamp diagonal movement
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        Vector3 movement = forward.normalized * deltaZ + right.normalized * deltaX;
+
         movement = Vector3.ClampMagnitude(movement, _speed);
-        
-        // Apply gravity after X and Z have been clamped
+
         movement.y = _gravity;
-        
+
         movement *= Time.deltaTime;
-        
-        // Convert movement vector to rotation settings of player
-        movement = transform.TransformDirection(movement);
-        
+
         _controller.Move(movement);
     }
 }
